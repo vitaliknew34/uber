366516686c82cfc1acb313c92e8be06e8a700fc2
vitaliknew34 <vitaliknew34@mail.ru>
2022-02-03 21:59:31 +0300

    урок 1

---
+++ javascript/src/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+let num = 5;
+const leftBorderWidtch = 1;
+
+num = 10;
+
+console.log(num);
+
+const isCheked = true,
+      isClose = true;
+
+console.log(isCheked && !isClose);
+// "&&"- логическое "И"
+// "||" - логическое "ИЛИ"
+// "!" - логическое "НЕ"
