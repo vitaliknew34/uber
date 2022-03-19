988f33cd770c24aba42dd87365cfa9c6d48f8d83
vitaliknew34 <vitaliknew34@mail.ru>
2022-03-20 00:11:55 +0300

    funcjs

--- javascript/src/js/script.js
+++ javascript/src/js/script.js
@@ -418,9 +418,23 @@ console.log(personalMovieDB); */
 
 //РЕШЕНИЕ:
 
+  /*   function twoWar(a,b) {
+        if ( a >= 0 && b >= 0)
+            return a - b;
+        if ( a < 0 && b < 0)
+            return a * b;
+        return a + b;
+    }
+    console.log(twoWar(3,2));
+    console.log(twoWar(-2,-3));
+    console.log(twoWar(2,-3));
+ */
+
+
     /* let a = 2;
     let b =  3;
-    if ( a > 0 && b > 0) {
+    
+    if ( a >= 0 && b >= 0) {
         alert(` a - b  = ${a - b}`)
     }
     if ( a < 0 && b < 0) {
@@ -435,7 +449,7 @@ console.log(personalMovieDB); */
 
 //РЕШЕНИЕ:
 
-     let a = ((Math.random() * 15).toFixed(0));
+     /* let a = ((Math.random() * 15).toFixed(0));
         switch (a) {
             case '1':
                 console.log(1)
@@ -470,7 +484,82 @@ console.log(personalMovieDB); */
                 break;
             
         } 
-    console.log(a)
+    console.log(a) */
     
 
 
+/*  5. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return. */
+
+//РЕШЕНИЕ:
+
+     function sum(a,b) {
+        return a + b;
+    }
+
+    function razn(a,b) {
+        return a - b;
+    }
+    razn();
+
+    function umn(a,b) {
+        return a * b;
+    }
+    umn();
+
+    function del(a,b) {
+        return a / b;
+    }
+    del(); 
+
+
+/* 6. Реализовать функцию с тремя параметрами: function mathOperation(arg1, arg2, operation), где arg1, arg2 – значения аргументов, operation – строка с названием операции. В зависимости от переданного значения операции выполнить одну из арифметических операций (использовать функции из пункта 5) и вернуть полученное значение (использовать switch). */
+
+//РЕШЕНИЕ:
+
+//ПРИМЕР №1
+
+    let arg1 = +prompt('Введите первое число' , '');
+    let arg2 = +prompt('Введите второе число' , '');
+    let operation = prompt('Введите знак + , - , * , / ', '');
+
+    function mathOperation(arg1, arg2, operation) {
+        switch (operation){
+            case '+':
+                alert(sum(arg1, arg2));
+                break;
+            case '-':
+                alert(razn(arg1, arg2));
+                break;
+            case '*' :
+                alert(umn(arg1, arg2));
+                break;
+            case '/':
+                alert(del(arg1, arg2));
+                break;
+            default:
+                alert('Знак введен не верно');
+                break;
+        }
+        
+    }
+
+    mathOperation(arg1, arg2, operation);
+    
+//ПРИМЕР №2
+    /* function mathOperation(arg1, arg2, operation) {
+        switch (operation){
+            case '+':
+                return sum(arg1,arg2);
+                break;
+            case '-':
+                return razn(arg1,arg2)
+                break;
+            case '*' :
+                return umn(arg1,arg2);
+                break;
+            case '/':
+                return del(arg1,arg2);
+                break;        
+        }
+    }
+    console.log(mathOperation(4, 3, '/' )); */
