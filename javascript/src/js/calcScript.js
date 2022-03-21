335481b9c71b828e225b7c82d9e6ef3ef4eb8126
vitaliknew34 <vitaliknew34@mail.ru>
2022-03-21 11:08:31 +0300

    calc

--- javascript/src/js/calcScript.js
+++ javascript/src/js/calcScript.js
@@ -49,3 +49,60 @@ else  {
 calc(); 
  /* let test = calc(); */
         /* alert(test); */
+
+        /*  5. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return. */
+
+//РЕШЕНИЕ:
+
+     function sum(a,b) {
+        return a + b;
+    }
+
+    function razn(a,b) {
+        return a - b;
+    }
+    razn();
+
+    function umn(a,b) {
+        return a * b;
+    }
+    umn();
+
+    function del(a,b) {
+        return a / b;
+    }
+    del(); 
+
+
+/* 6. Реализовать функцию с тремя параметрами: function mathOperation(arg1, arg2, operation), где arg1, arg2 – значения аргументов, operation – строка с названием операции. В зависимости от переданного значения операции выполнить одну из арифметических операций (использовать функции из пункта 5) и вернуть полученное значение (использовать switch). */
+
+//РЕШЕНИЕ:
+
+//ПРИМЕР №1
+
+    let arg1 = +prompt('Введите первое число' , '');
+    let arg2 = +prompt('Введите второе число' , '');
+    let operation = prompt('Введите знак + , - , * , / ', '');
+
+    function mathOperation(arg1, arg2, operation) {
+        switch (operation){
+            case '+':
+                alert(sum(arg1, arg2));
+                break;
+            case '-':
+                alert(razn(arg1, arg2));
+                break;
+            case '*' :
+                alert(umn(arg1, arg2));
+                break;
+            case '/':
+                alert(del(arg1, arg2));
+                break;
+            default:
+                alert('Знак введен не верно');
+                break;
+        }
+        
+    }
+
+    mathOperation(arg1, arg2, operation);
